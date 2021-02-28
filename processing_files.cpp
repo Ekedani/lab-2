baa0266daa83fa90109fb66cffe702b74606b359
Vladlen <ilusenkovladlen@gmail.com>
2021-03-01 00:44:05 +0200

    Обновление функции вывода, комментарии

--- processing_files.cpp
+++ processing_files.cpp
@@ -22,6 +22,7 @@ void processTheFile(string pathToFile, vector<Country*> AllCountries) {
     inFile.close();
 }
 
+//TODO: Исправить баг с передачей пустой строки в функцию
 void processTheLine(string currentLine, vector<Country*> AllCountries){
     //Создание элемента в векторе стран
     AllCountries.push_back(new Country());
@@ -55,10 +56,21 @@ void sortTheVector(vector<Country*> AllCountries){
 void outputTheResult(vector<Country*> AllCountries){
     //Создание файла на запись
     ofstream outFile;
+    //TODO: проверь правильно ли будет создаваться файл
     outFile.open("result.csv");
 
     //Запись результатов в файл
     //TODO: нужно согласовать в каком порядке и что мы собираемся выводить
+    //UPD: Балл,Страна,КоличествоГолосов
+    //Баллы: 12, 10, 8, 7, 6, 5, 4, 3, 2, 1
+    for(int countryNum = 0; countryNum < 10; countryNum++){
+        //TODO: сделай так, чтобы в файл выводились нужные баллы
+        int mark = 12;
+
+        //TODO: проверь правильный ли будет вывод в файле result по такому принципу
+        outFile << mark << "," << AllCountries[countryNum]->name << "," << AllCountries[countryNum]->score << endl;
+    }
+    //TODO: проверь результат: если он не будет совпадать с итогом, то обрати внимание на передачу вектора в функции
 
     //Закрытие файла
     outFile.close();
